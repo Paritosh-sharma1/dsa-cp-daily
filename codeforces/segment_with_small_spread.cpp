f2133ce02f1e66fe84d893cb6a78aa681cecdbe5
Paritosh <paritoshsharma2020@gmail.com>
2025-09-16 16:44:21 +0530

    Added Solution

---
+++ codeforces/segment_with_small_spread.cpp
@@ -0,0 +1,182 @@
+/*
+    Author : Paritosh Sharma    
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// Speed
+#define fastio() ios::sync_with_stdio(0); cin.tie(0);
+
+// Macros
+#define endl "\n"
+#define no  cout << "NO" << endl;
+#define yes cout << "YES" << endl;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for (int i = a; i < b; i++)
+#define rep(i,a,b) for(int i=1;i<=b;i++)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+// Enable Debug
+#ifndef ONLINE_JUDGE
+    #define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
+#else
+    #define debug(x)
+#endif
+
+// Constants
+const int MOD = 1e9 + 7;
+
+// Typedefs
+typedef long double ld;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+typedef map<char, int> mci;
+typedef set<int> st;
+
+// Debugging print functions
+#define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(double t) { cerr << t; }
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.f); cerr << ","; _print(p.s); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+//Maths
+bool isprime(int x) {
+    if (x <= 1) return false;
+    for (int i = 2; i * i <= x; i++)
+        if (x % i == 0) return false;
+    return true;
+}
+
+void printFactors(int n, vector<int>& fact) {
+    for (int i = 1; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            fact.push_back(i);
+            if (i != n / i)
+                fact.push_back(n / i);
+        }
+    }
+}
+long long lcm(ll a, ll b) {
+    return (1LL * a * b) / __gcd(a, b);
+}
+
+bool isPerfectSquare(long long n) { 
+    long long root = sqrt(n);
+    return root * root == n;
+}
+
+bool isq(ll n)
+{
+    return ceil(sqrtl(n)) == floor(sqrtl(n));
+}
+
+long long fact(int n) {
+    long long result = 1;
+    for (int i = 2; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
+}
+// 1. Practice
+// 2. Solve problems with editorials
+// 3. Solving problems slightly above your level. 
+// 4. Solve an optimal numbers of problems like 200-400 not 2000-4000
+// 5. Don't get stuck for long hours
+
+// push O(1), pop O(1) ,min O(1),max O(1), sliding window O(n)
+struct stack{
+    vector<ll> s,smin={LLONG_MAX},smax={LLONG_MIN};
+    void push(ll x){
+        s.push_back(x);
+        smin.push_back(::min(smin.back(),x));
+        smax.push_back(::max(smax.back(),x));
+    }
+    ll pop(){
+        ll res = s.back();
+        s.pop_back();
+        smin.pop_back();
+        smax.pop_back();
+        return res;
+    }
+    bool empty(){
+        return s.empty();
+    }
+    ll min(){
+        return smin.back();
+    }
+    ll max(){
+        return smax.back();
+    }
+};
+::stack s1,s2;
+void add(ll x){
+    s2.push(x);
+}
+void remove(){
+    if(s1.empty()){
+        while(!s2.empty()){
+            s1.push(s2.pop());
+        }
+    }
+    s1.pop();
+}
+ll k;
+bool good(){
+    ll mn = min(s1.min(),s2.min());
+    ll mx= max(s1.max(),s2.max());
+    return mx-mn<=k;
+}
+void paritosh() {
+    ll n;
+    cin >> n >> k;
+    vl arr(n);
+    REP(i,0,n) cin >> arr[i];
+
+    ll j=0;
+    ll res=0;
+    REP(i,0,n){
+        add(arr[i]);
+        while(!good()){
+            remove();
+            j++;
+        }
+        res+=i-j+1;
+    }
+    cout<<res<<endl;
+}
+
+int main() {
+    fastio();
+    freopen("error.txt", "w", stderr);
+    // int t;
+    // cin >> t;
+    // while (t--) {
+    //     paritosh();
+    // }
+    paritosh();
+    return 0;
+}
