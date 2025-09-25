d9697262e519d146bf1a617e4bb5045da2d48991
Paritosh <paritoshsharma2020@gmail.com>
2025-09-25 15:55:55 +0530

    Added Solution

---
+++ Leetcode/House Robber.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+
+        // vector<int> dp(n, 0);
+        // dp[0] = nums[0];
+        int prev2=0,curi=0;
+        int prev=nums[0];
+        for (int i = 1; i < n; ++i) {
+            int pick = nums[i];
+            if (i > 1) pick +=prev2;
+            int notpick = prev;
+            curi = max(pick, notpick);
+            prev2=prev;
+            prev=curi;
+        }
+        return prev;
+    }
+};
