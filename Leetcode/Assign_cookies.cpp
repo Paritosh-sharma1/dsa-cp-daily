ac3266ef2bf202e4d20f60dd32dae7a40b4705ea
Paritosh <paritoshsharma2020@gmail.com>
2025-10-17 17:25:19 +0530

    Added Solution

---
+++ Leetcode/Assign_cookies.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int i=0,j=0,cnt=0,n=g.size(),m=s.size();
+        while(i<n&& j<m){
+            if(s[j]>=g[i]){
+                cnt++;
+                i++;
+                j++;
+            }
+            else 
+            j++;
+        }
+        return cnt;
+    }
+};
