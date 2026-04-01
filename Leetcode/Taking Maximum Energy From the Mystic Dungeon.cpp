aa76e4d19fc1add38982fd80097ab3ca446a9d34
Paritosh <paritoshsharma2020@gmail.com>
2026-04-01 23:03:31 +0530

    Added implementation for Taking Maximum Energy From the Mystic Dungeon problem

---
+++ Leetcode/Taking Maximum Energy From the Mystic Dungeon.cpp
@@ -0,0 +1,20 @@
+Link: https://leetcode.com/problems/taking-maximum-energy-from-the-mystic-dungeon/description/
+
+
+class Solution {
+public:
+    int maximumEnergy(vector<int>& energy, int k) {
+        int n = energy.size();
+        int ans = INT_MIN;
+
+        for(int i=n-k;i<n;i++){
+            int sum=0;
+            for(int j=i;j>=0;j-=k){
+                sum+=energy[j];
+                ans=max(ans,sum);
+            }
+        }
+
+        return ans;
+    }
+};
